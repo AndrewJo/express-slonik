7504c0b9663dbd9303955bf8749b6c3b1e1653c8
Andrew Jo <dev@andrewjo.com>
2023-01-29 00:01:20 -0800

    feat: export convenience sql tag that includes void type alias

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { DatabaseTransactionConnection } from "slonik";
+import { createSqlTag, DatabaseTransactionConnection } from "slonik";
+import { z } from "zod";
 
 import createMiddleware from "express-slonik/middleware";
 import { IsolationLevels } from "express-slonik/middleware";
@@ -13,6 +14,12 @@ declare global {
     }
   }
 }
+
+export const sql = createSqlTag({
+  typeAliases: {
+    void: z.object({}).strict(),
+  },
+});
 
 export { IsolationLevels };
 export type { IsolationLevel, RequestTransactionContext as SlonikRequestContext };
